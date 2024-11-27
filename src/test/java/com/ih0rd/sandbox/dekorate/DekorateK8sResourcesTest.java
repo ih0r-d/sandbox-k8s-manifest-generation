b1c1a47ab3c5541377d3532fb0c213d64c78acc8
Ihor Didyk <ihordidyk95@gmail.com>
2024-11-27 22:00:15 +0200

    dekorate: helm charts
    Signed-off-by: Ihor Didyk <ihordidyk95@gmail.com>

--- src/test/java/com/ih0rd/sandbox/dekorate/DekorateK8sResourcesTest.java
+++ src/test/java/com/ih0rd/sandbox/dekorate/DekorateK8sResourcesTest.java
@@ -1,5 +1,9 @@
 package com.ih0rd.sandbox.dekorate;
 
+import io.dekorate.helm.model.Chart;
+import io.dekorate.helm.model.HelmDependency;
+import io.dekorate.helm.model.ValuesSchema;
+import io.dekorate.helm.model.ValuesSchemaProperty;
 import io.fabric8.certmanager.api.model.v1.Certificate;
 import io.fabric8.certmanager.api.model.v1.Issuer;
 import io.fabric8.kubernetes.api.model.*;
@@ -7,7 +11,10 @@ import io.fabric8.kubernetes.api.model.apps.Deployment;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,11 +24,14 @@ public class DekorateK8sResourcesTest {
     private static final Integer EXPECTED_K8S_ITEMS = 6;
     private static List<String> EXPECTED_HOST_NAME_1;
     private static List<String> EXPECTED_HOST_NAME_2;
-    private static  String EXPECTED_VOLUME_NAME;
-    private static  String EXPECTED_SECRET_NAME ;
-    private static  String HTTPS_SCHEMA ;
+    private static String EXPECTED_VOLUME_NAME;
+    private static String EXPECTED_SECRET_NAME;
 
     public static KubernetesList K8S_LIST;
+
+    private static String CHART_NAME = "myChart";
+    private static String CHART_OUTPUT_LOCATION;
+    private static String ROOT_CONFIG_NAME;
 
     @BeforeAll
     static void setupAll() {
@@ -30,13 +40,15 @@ public class DekorateK8sResourcesTest {
         EXPECTED_HOST_NAME_2 = List.of("foo.org", "bar.com");
         EXPECTED_VOLUME_NAME = "volume-certs";
         EXPECTED_SECRET_NAME = "tls-secret";
-        HTTPS_SCHEMA = "HTTPS";
+        CHART_NAME = "myChart";
+        CHART_OUTPUT_LOCATION = "META-INF/dekorate/helm/kubernetes/" + CHART_NAME;
+        ROOT_CONFIG_NAME = "app";
     }
 
     @Test
     public void shouldContainConfigMap() {
         assertNotNull(K8S_LIST);
-        assertEquals(EXPECTED_K8S_ITEMS, K8S_LIST.getItems().size());
+        assertEquals(7, K8S_LIST.getItems().size());
         Deployment deployment = SharedUtils.findFirst(Deployment.class);
         assertEquals("Deployment", deployment.getKind());
         final Volume firstVolume = deployment.getSpec().getTemplate().getSpec().getVolumes().getFirst();
@@ -48,7 +60,7 @@ public class DekorateK8sResourcesTest {
     @Test
     public void shouldUseCustomImageName() {
         assertNotNull(K8S_LIST);
-        assertEquals(EXPECTED_K8S_ITEMS, K8S_LIST.getItems().size());
+        assertEquals(7, K8S_LIST.getItems().size());
 
         final Deployment deployment = SharedUtils.findFirst(Deployment.class);
         assertNotNull(deployment);
@@ -61,7 +73,7 @@ public class DekorateK8sResourcesTest {
     @Test
     public void shouldContainHostAliases() {
         assertNotNull(K8S_LIST);
-        assertEquals(SharedUtils.EXPECTED_K8S_ITEMS, K8S_LIST.getItems().size());
+        assertEquals(7, K8S_LIST.getItems().size());
         final Deployment deployment = SharedUtils.findFirst(Deployment.class);
 
         final List<HostAlias> hostAliases = deployment.getSpec().getTemplate().getSpec().getHostAliases();
@@ -102,13 +114,189 @@ public class DekorateK8sResourcesTest {
                 .anyMatch(v -> v.getName().equals(EXPECTED_VOLUME_NAME) && v.getSecret().getSecretName().equals(EXPECTED_SECRET_NAME)));
         assertTrue(containers.stream()
                 .allMatch(c -> c.getVolumeMounts().stream().anyMatch(m -> m.getName().equals(EXPECTED_VOLUME_NAME))));
-        assertTrue(containers.stream()
-                .map(Container::getReadinessProbe)
-                .filter(Objects::nonNull)
-                .allMatch(probe -> probe.getHttpGet().getScheme().equals(HTTPS_SCHEMA)));
-        assertTrue(containers.stream()
-                .map(Container::getLivenessProbe)
-                .filter(Objects::nonNull)
-                .allMatch(probe -> probe.getHttpGet().getScheme().equals(HTTPS_SCHEMA)));
+
+        final List<Probe> readinessProbe = containers.stream().map(Container::getReadinessProbe).toList();
+        assertNotNull(readinessProbe);
+        assertFalse(readinessProbe.isEmpty());
+
+        final List<Probe> livenessProbe = containers.stream().map(Container::getLivenessProbe).toList();
+        assertNotNull(livenessProbe);
+        assertFalse(livenessProbe.isEmpty());
     }
+
+
+    @Test
+    public void shouldHelmManifestsBeGenerated() throws IOException {
+        Chart chart = SharedUtils.read(CHART_OUTPUT_LOCATION +"/Chart.yaml", Chart.class);
+        assertNotNull(chart, "Chart is null!");
+        assertEquals("v2", chart.getApiVersion());
+        // Should be the same as in `dekorate.helm.chart` from properties.
+        assertEquals(CHART_NAME, chart.getName());
+        // Should contain expected dependencies
+        final List<HelmDependency> dependencies = chart.getDependencies();
+        assertNotNull(dependencies);
+        assertEquals(3, dependencies.size());
+        assertEquals("dependencyNameA", dependencies.getFirst().getName());
+        assertEquals("dependencyNameA", dependencies.getFirst().getAlias());
+        assertEquals("0.0.1", dependencies.get(0).getVersion());
+        assertEquals("http://localhost:8080", dependencies.get(0).getRepository());
+        assertEquals("dependencyNameB", dependencies.get(1).getName());
+        assertEquals("app", dependencies.get(1).getAlias());
+        assertEquals("dependencyNameC", dependencies.get(2).getName());
+        assertEquals("app.database.enabled", dependencies.get(2).getCondition());
+        assertEquals(2, dependencies.get(2).getTags().length);
+        assertEquals("web", dependencies.get(2).getTags()[0]);
+        assertEquals("frontend", dependencies.get(2).getTags()[1]);
+        // Values.yaml manifest
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/values.yaml"));
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/values.dev.yaml"));
+        // templates
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/templates/deployment.yaml"));
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/templates/ingress.yaml"));
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/templates/service.yaml"));
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/templates/_helpers.tpl"));
+        // charts folder
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/charts"));
+        // notes
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/templates/NOTES.txt"));
+        // optional resources
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/LICENSE"));
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/README.md"));
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/values.schema.json"));
+        // crds
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/crds"));
+        assertNotNull(SharedUtils.loadKubernetesResources(CHART_OUTPUT_LOCATION + "/crds/crontabs.stable.example.com.yaml"));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void chartsShouldContainExpectedData() throws IOException {
+        Map<String, Object> chart = SharedUtils.read(CHART_OUTPUT_LOCATION + "/Chart.yaml", Map.class);
+        assertNotNull(chart, "Chart.yaml is null!");
+
+        assertTrue(chart.containsKey("annotations"), "Does not contain `annotations` from the user Charts.yml!");
+        assertEquals(CHART_NAME, chart.get("name"), "The name was not replaced with the generated value!");
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void valuesShouldContainExpectedData() throws IOException {
+        Map<String, Object> values = SharedUtils.read(CHART_OUTPUT_LOCATION + "/values.yaml", Map.class);
+        assertNotNull(values, "Values is null!");
+
+        assertNotNull(values.containsKey(ROOT_CONFIG_NAME), "Does not contain `" + ROOT_CONFIG_NAME + "`");
+        assertNotNull(values.get(ROOT_CONFIG_NAME) instanceof Map, "Value `" + ROOT_CONFIG_NAME + "` is not a map!");
+
+        // Rootless properties
+        assertEquals("rootless-property", values.get("prop"));
+
+        Map<String, Object> app = (Map<String, Object>) values.get(ROOT_CONFIG_NAME);
+        // Should contain image
+        assertNotNull(app.get("image"));
+        // Should contain replicas
+        assertEquals(3, app.get("replicas"));
+        // Should NOT contain notFound: as this property is ignored
+        assertNull(app.get("notFound"));
+        // Should contain vcsUrl with the overridden value from properties
+        assertEquals("Overridden", app.get("vcsUrl"));
+        // Should map ports:
+        Map<String, Object> ports = (Map<String, Object>) app.get("ports");
+        assertNotNull(ports);
+        assertEquals(8080, ports.get("http"));
+        // Should include health check properties:
+        // 1. tcp socket action
+        Map<String, Object> livenessValues = (Map<String, Object>) app.get("livenessProbe");
+        assertProbe(livenessValues, 11, 31);
+        Map<String, Object> tcpSocketValues = (Map<String, Object>) livenessValues.get("tcpSocket");
+        assertEquals("1111", tcpSocketValues.get("port"));
+        assertEquals("my-service", tcpSocketValues.get("host"));
+        // 2. http get action
+        Map<String, Object> readinessValues = (Map<String, Object>) app.get("readinessProbe");
+        assertProbe(readinessValues, 10, 30);
+        Map<String, Object> httpGetValues = (Map<String, Object>) readinessValues.get("httpGet");
+        assertEquals("/readiness", httpGetValues.get("path"));
+        // It should be null because it's now mapped with app.ports.http;
+        assertNull(httpGetValues.get("port"));
+        assertEquals("HTTP", httpGetValues.get("scheme"));
+        // 3. exec action
+        Map<String, Object> startupValues = (Map<String, Object>) app.get("startupProbe");
+        assertProbe(startupValues, 12, 32);
+        Map<String, Object> execValues = (Map<String, Object>) startupValues.get("exec");
+        List<String> command = (List<String>) execValues.get("command");
+        assertEquals(2, command.size());
+        assertEquals("command1", command.get(0));
+        assertEquals("command2", command.get(1));
+        // 4. helm expression
+//        assertEquals(SharedUtils.readString("expected-ingress.yaml"), SharedUtils.readString(CHART_OUTPUT_LOCATION + "/templates/ingress.yaml"));
+//        assertEquals(SharedUtils.readString("expected-configmap.yaml"), SharedUtils.readString(CHART_OUTPUT_LOCATION + "/templates/configmap.yaml"));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void valuesShouldContainExpectedDataInDevProfile() throws IOException {
+        Map<String, Object> values = SharedUtils.read(CHART_OUTPUT_LOCATION + "/values.dev.yaml", Map.class);
+        assertNotNull(values, "Values is null!");
+
+        assertTrue(values.containsKey(ROOT_CONFIG_NAME), "Does not contain `" + ROOT_CONFIG_NAME + "`");
+        assertInstanceOf(Map.class, values.get(ROOT_CONFIG_NAME), "Value `" + ROOT_CONFIG_NAME + "` is not a map!");
+        Map<String, Object> helmExampleValues = (Map<String, Object>) values.get(ROOT_CONFIG_NAME);
+
+        // Should contain image
+        assertNotNull(helmExampleValues.get("image"));
+        // Should contain replicas
+        assertEquals(3, helmExampleValues.get("replicas"));
+        // Should NOT contain notFound: as this property is ignored
+        assertNull(helmExampleValues.get("notFound"));
+        // Should contain vcsUrl with the value from properties
+        assertEquals("Only for DEV!", helmExampleValues.get("vcsUrl"));
+        // Should contain ingress with the value from properties
+        assertEquals("my-test-host", helmExampleValues.get("host"));
+    }
+
+    @Test
+    public void valuesFileShouldContainDependencyValues() throws IOException {
+        ValuesSchema schema = SharedUtils.read(CHART_OUTPUT_LOCATION + "/values.schema.json", ValuesSchema.class);
+        // From properties
+        assertEquals("My Values", schema.getTitle());
+        // From the provided values schema json
+        assertEquals(2, schema.getRequired().size());
+        Iterator<String> requirements = schema.getRequired().iterator();
+        assertEquals("protocol", requirements.next());
+        assertEquals("port", requirements.next());
+        ValuesSchemaProperty image = schema.getProperties().get("image");
+        assertNotNull(image);
+        assertEquals("Container Image", image.getDescription());
+        assertEquals(2, image.getProperties().size());
+        // From config references
+        ValuesSchemaProperty app = schema.getProperties().get("app");
+        assertNotNull(app);
+        ValuesSchemaProperty replicas = app.getProperties().get("replicas");
+        assertNotNull(replicas);
+        assertEquals(3, replicas.getMinimum());
+        assertEquals(5, replicas.getMaximum());
+        assertEquals("Overwrite default description!", replicas.getDescription());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void validateValuesSchemaFile() throws IOException {
+        Map<String, Object> values = SharedUtils.read(CHART_OUTPUT_LOCATION + "/values.yaml", Map.class);
+        Map<String, Object> dependencyA = (Map<String, Object>) values.get("dependencyNameA");
+        assertEquals("aValue", dependencyA.get("config"));
+
+        Map<String, Object> dependencyApp = (Map<String, Object>) values.get("app");
+        Map<String, Object> config = (Map<String, Object>) dependencyApp.get("config");
+        assertEquals("John", config.get("user"));
+        assertEquals("mysql", config.get("database"));
+    }
+
+    private void assertProbe(Map<String, Object> probeValues, int expectedTimeoutSeconds, int expectedPeriodSeconds) {
+        assertEquals(3, probeValues.get("failureThreshold"));
+        assertEquals(expectedTimeoutSeconds, probeValues.get("timeoutSeconds"));
+        assertEquals(expectedPeriodSeconds, probeValues.get("periodSeconds"));
+        assertEquals(1, probeValues.get("successThreshold"));
+        assertEquals(0, probeValues.get("initialDelaySeconds"));
+    }
+
+
 }
--- src/test/java/com/ih0rd/sandbox/dekorate/SharedUtils.java
+++ src/test/java/com/ih0rd/sandbox/dekorate/SharedUtils.java
@@ -4,13 +4,18 @@ import io.dekorate.utils.Serialization;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.KubernetesList;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -22,6 +27,10 @@ public class SharedUtils {
 
     public static KubernetesList loadKubernetesResources() {
         return Serialization.unmarshalAsList(Objects.requireNonNull(SharedUtils.class.getClassLoader().getResourceAsStream(K8S_MANIFEST)));
+    }
+
+    public static InputStream loadKubernetesResources(String path) {
+        return SharedUtils.class.getClassLoader().getResourceAsStream(path);
     }
 
     @SuppressWarnings("unchecked")
@@ -42,5 +51,17 @@ public class SharedUtils {
         } catch (IOException | InterruptedException e) {
             return Optional.empty();
         }
+    }
+
+    public static <T> T read(String path, Class<T> clazz) throws IOException {
+        return Serialization.yamlMapper().readValue(SharedUtils.class.getClassLoader()
+                .getResourceAsStream(path), clazz);
+    }
+
+    public static String readString(String path) {
+        return Objects.requireNonNull(new BufferedReader(
+                new InputStreamReader(SharedUtils.loadKubernetesResources(path), StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.joining(System.lineSeparator())));
     }
 }
